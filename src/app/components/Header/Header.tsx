b657c94a0cbfe6cd91d69be54e5a849ef089ebc3
will-santosx <o.santxz@proton.me>
2024-06-24 18:08:13 -0400

    header responsivo para todos dispositivos

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import LogoHorizontal from "@/app/assets/brand/logo-green-horizontal.png";
-import LogoIcon from "@/app/assets/brand/logo-green-icon.png";
 import { BiMenu, BiX } from "react-icons/bi";
 import { useState } from "react";
 
@@ -18,61 +17,62 @@ export default function Header() {
   };
 
   return (
-    <header className="bg-white w-full sm:px-[15px] sm:h-[55px] md:h-[100px] lg:h-[6.25rem] lg:px-[160px] flex justify-center items-center font-medium">
-      <div className="w-full flex justify-between items-center ">
+    <header className="bg-white w-[100vw] flex items-center font-medium | max-sm:px-[20px] max-sm:h-[55px] | sm:px-[20px] sm:h-[75px] | lg:px-[30px]">
+      <div className="w-full flex items-center justify-around | max-sm:gap-[27px] | sm:gap-[130px]">
         <button
-          className="sm:hidden outline-none p-1 focus:bg-neutral-rich-black focus:bg-opacity-20 rounded-full"
+          className="lg:hidden | outline-none focus:bg-neutral-rich-black focus:bg-opacity-20 rounded-normal"
           onClick={toggleMenu}
         >
-          <BiMenu className="text-secondary-cadmium-green w-[24px] h-[24px]" />
+          <BiMenu className="sm:w-[35px] sm:h-[35px] | text-secondary-cadmium-green w-[24px] h-[24px]" />
         </button>
         <div className="flex">
           <Link href={"/"}>
             <Image
               alt="Logo da Willy Academy Verde no Sentido Horizontal."
               src={LogoHorizontal}
-              className="max-lg:w-[219px] lg:min-w-[200px] sm:w-[90px] max-sm:w-[100px] md:w-[160px]"
+              className="max-sm:w-[130px] | sm:w-[170px] | min-w-[130px]"
             />
           </Link>
         </div>
-        <nav className="hidden sm:text-sm-small md:text-small lg:text-small whitespace-nowrap sm:flex items-center lg:gap-[40px] gap-5 text-neutral-rich-black">
+        <nav className="lg:flex lg:gap-[20px] | xl:gap-[50px] | hidden  whitespace-nowrap items-center text-neutral-rich-black">
           <Link
             href={"#"}
-            className="transition-all hover:underline hover:opacity-85"
+            className="transition-all nav-links pb-1 hover:text-black"
           >
             Banco de questões
           </Link>
           <Link
             href={"#"}
-            className="transition-all hover:underline hover:opacity-85"
+            className="transition-all nav-links pb-1 hover:text-black"
           >
             Disciplinas
           </Link>
           <Link
             href={"#"}
-            className="transition-all hover:underline hover:opacity-85"
+            className="transition-all nav-links pb-1 hover:text-black"
           >
             Desempenho
           </Link>
           <Link
             href={"#"}
-            className="transition-all hover:underline hover:opacity-85"
+            className="transition-all nav-links pb-1 hover:text-black"
           >
             Sobre Nós
           </Link>
         </nav>
-        <div className="hidden sm:block">
-          <Link href={"#"}>
-            <button className="bg-secondary-cadmium-green lg:w-[320px] w-[130px] md:w-[164px] h-[30px] md:h-[45px] lg:h-[65px] transition-all hover:scale-105 hover:bg-selected-secondary font-semi-bold uppercase lg:text-normal rounded-normal text-primary-duch-white">
-              Acesse
-            </button>
-          </Link>
-        </div>
+        <Link
+          href={"#"}
+          className="max-sm:w-[105px] max-sm:h-[33px] | sm:w-[145px] sm:h-[42px] | lg:text-[19px] lg:w-[190px] | xl:text-[20px] xl:w-[190px] xl:h-[55px] | text-[16px]"
+        >
+          <button className="w-full h-full bg-secondary-cadmium-green transition-all hover:scale-105 hover:bg-selected-secondary font-semi-bold uppercase rounded-normal text-primary-duch-white">
+            Acesse
+          </button>
+        </Link>
       </div>
       {menuOpen && (
         <div className="fixed inset-0 bg-neutral-bright-gray z-50 flex flex-col items-center justify-center p-4">
           <button
-            className="absolute top-4 right-4 outline-none p-1 focus:bg-neutral-rich-black focus:bg-opacity-20 rounded-full"
+            className="absolute top-4 right-4 outline-none p-1 focus:bg-neutral-rich-black focus:bg-opacity-20 rounded-normal"
             onClick={closeMenu}
           >
             <BiX className="text-secondary-cadmium-green w-[24px] h-[24px]" />
@@ -89,11 +89,6 @@ export default function Header() {
             </Link>
             <Link href={"#"} onClick={closeMenu}>
               Sobre Nós
-            </Link>
-            <Link href={"#"} onClick={closeMenu} className="mt-4">
-              <button className="bg-secondary-cadmium-green w-[160px] h-[45px] transition-all hover:scale-105 hover:bg-selected-secondary font-semi-bold uppercase text-normal rounded-normal text-primary-duch-white">
-                Acesse
-              </button>
             </Link>
           </nav>
         </div>
